381d66b19476ebd4d8404df8a938851260da8737
tanpeterson <tanpeterson@gmail.com>
2021-11-22 13:55:12 -0800

    first

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <p>
+        Svelte is an incredibly performant web framework. Deno is a secure runtime with built-in TypeScript support. 
+        Yet neither of these powerful technologies have out-of-the-box support with one another. 
+      </p>  
+      <p>
+        NOVAS's build-tool bridges the gap of implementing and integrating Svelte and Deno together, so you can start creating more with less work.
+      </p>
+      <hr/>
+    </div>
+  )
+}
---
+++ src/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Intro() {
+  return (
+    <div>
+      <p>logo 
+        <h1>NOVAS</h1>
+      </p>
+      <p>A build tool for Svelte applications in Deno.</p>
+      <hr/>
+    </div>
+  )
+}
