dac6eb22d101b79c6fa9179a5559f56e0a9b47b6
tanpeterson <tanpeterson@gmail.com>
2021-11-22 15:25:31 -0800

    changes2

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className='Navbar'>
-      <span className='AppName'>🌟NOVAS</span>
+      <span className='AppName'>NOVAS</span>
       <div>
         <a href="https://github.com/NOVASland/NOVAS" target="_blank"><img className='navbar-githublogo' src="/images/githublogo.png" alt='deno'/>      </a>
         <a href="https://deno.land/x/novas" target="_blank"><img className='navbar-denologo' src="/images/denologo.png" alt='deno'/></a>      
